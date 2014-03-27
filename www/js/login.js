097eef43d649db4a1b2a640095b7f2eac5b8b6fa
rgold1 <rebecca@eevee.(none)>
2014-03-27 17:57:13 -0500

    Adding some shell javascript code to set up the selectGameScreen.html so it's dynamic

---
+++ www/js/login.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $("#loginForm").submit(function(event) {
+        var $form = $(this);
+        var u = $("#username").val();
+        var p = $("#password").val();
+
+        $.ajax({
+            url : "http://localhost:8080/rest_login",
+            type: "POST",
+            data : {username: u, password: p},
+            contentType:'application/json; charset=UTF-8',
+            dataType: 'json',
+            success: function(data) {
+                if (data.status === true) {
+                    document.location='./selectGameScreen.html';
+                } else {
+                    alert(data.status);
+                }
+            },
+            error: function () {
+                console.log("Something's fundamentally wrong");
+            }
+        });
+
+        event.preventDefault();
+    });
+
+});
+
+
+
+//function init()   {
+//    document.addEventListener("deviceready", deviceReady, true);
+//    delete init;
+//}
