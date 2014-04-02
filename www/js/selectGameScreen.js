2311375ac67bf8004d02369b66fc604ef04b12af
rgold1 <rebecca@eevee.(none)>
2014-04-02 13:00:36 -0500

    Got the div working dynmaically.

--- www/js/selectGameScreen.js
+++ www/js/selectGameScreen.js
@@ -2,12 +2,14 @@ $(document).ready(function() {
 
    var games = {
    		BattleRoyal : 20,
-   		MyGame : 5	
+   		MyGame : 5,	
+         AnotherGame : 8,
+         YetAnotherGame: 60,
+         Game : 15
    };  
+
    for (var game in games){
    		if(games.hasOwnProperty(game)){
-   			// $("#gamePannel").append("<div class=\"text box\">Test</div>");
-   			// $("#gamePannel").append("<div/>", {class: ".text.box"});
    		   var $div = $("<div>", {id: game, class: "text box", text: game});
             $("#gamePannel").append($div);
 
