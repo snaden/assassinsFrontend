594b7d8eec5e7b8b8e598d4e06c4bdd58bc0cfa1
rgold1 <rebecca@eevee.(none)>
2014-04-02 12:47:23 -0500

    Got the divs dynamically generating and working!

--- www/js/selectGameScreen.js
+++ www/js/selectGameScreen.js
@@ -6,8 +6,12 @@ $(document).ready(function() {
    };  
    for (var game in games){
    		if(games.hasOwnProperty(game)){
-   			$("#gamePannel").append("<div class=\"text box\">Test</div>");
-   		}
+   			// $("#gamePannel").append("<div class=\"text box\">Test</div>");
+   			// $("#gamePannel").append("<div/>", {class: ".text.box"});
+   		   var $div = $("<div>", {id: game, class: "text box", text: game});
+            $("#gamePannel").append($div);
+
+         }
    }
    
 });
