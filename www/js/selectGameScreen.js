56fe53a7730a39f2009b0da8f871a7e19c7d5ebd
rgold1 <rebecca@eevee.(none)>
2014-04-09 21:18:27 -0500

    Prettying thins up and organizing so we can coordinate.

--- www/js/selectGameScreen.js
+++ www/js/selectGameScreen.js
@@ -2,9 +2,18 @@ $(document).ready(function() {
 
    var games;
 
+   // $.ajax({
+   //    url: "http://localhost:8080/api/list_games",
+   //    type: "GET",
+   //    contentType:'application/json; charset=UTF-8',
+   //    dataType: 'json',
+   //    success: function(response) {
+   //                games = response;         
+   //             }
+   // });
+
    $.getJSON("http://localhost:8080/api/list_games", function( response ){
       games = response;
-      console.log(games);
       for (var game in games){
             if(games.hasOwnProperty(game)){
                var $div = $("<div>", {id: game, class: "text box", text: games[game].title});
