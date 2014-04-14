6d7d0773957760ac2594a6ebab92c94c424e71c3
keeeeenw <wangxiaotian2007@gmail.com>
2014-04-14 12:43:45 -0500

    fixed the error that prevent the page from redirecting

--- platforms/ios/www/js/login.js
+++ platforms/ios/www/js/login.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+    //redirect if the user is already logged in
+       var user = localStorage.getItem("user");
+       if(user != null){ //user does exist!
+         document.location='./selectGameScreen.html';
+       } 
+
     $("#loginForm").submit(function(event) {
 
         var $form = $(this);
@@ -6,19 +12,24 @@ $(document).ready(function() {
         var p = $("#password").val();
 
         $.ajax({
-            url : "http://localhost:8080/api/rest_login",
+            url : _app_base+"/api/rest_login",
             type: "POST",
             data : JSON.stringify({username: u, password: p}),
             contentType:'application/json; charset=UTF-8',
             dataType: 'json',
             success: function(data) {
                 if (data.status === true) {
+                    //Puting user in localStorage
+                    localStorage.setItem("user", u);
+                    //Redirect to the selectGameScreen page
                     document.location='./selectGameScreen.html';
                 } else {
+                    localStorage.removeItem("user");
                     alert(data.status);
                 }
             },
             error: function () {
+                localStorage.removeItem("user");
                 console.log("Something's fundamentally wrong");
             }
         });
--- platforms/ios/www/js/selectGameScreen.js
+++ platforms/ios/www/js/selectGameScreen.js
@@ -1,17 +1,43 @@
 $(document).ready(function() {
 
-   var games;
+   //Check whether the user is there
+   var user = localStorage.getItem("user");
+   console.log(user);
+   if(user == null){ //user does not exist, bad!
+     document.location='./login.html';
+   } 
 
-   $.getJSON("http://localhost:8080/api/list_games", function( response ){
-      games = response;
+   //take a look at underscore.js for the utility functions
+   var request_url = _app_base+"/api/list_games";
+   var request_data = {username:user};
+   $.getJSON(request_url, request_data, function(response){
+      var games = response;
       console.log(games);
       for (var game in games){
             if(games.hasOwnProperty(game)){
-               var $div = $("<div>", {id: game, class: "text box", text: games[game].title});
-               $("#gamePannel").append($div);
+               var anchor = $("<a>", {id: game, class: "game_item", text: games[game].title, href: './gameStatsScreen.html'})
+               var div = $("<div>", {class: "text box"});
+               div.append(anchor);
+               $("#gamePannel").append(div);
+               //We class need to setup the click listener
+               setupGameStatsRedirect()
             }
       } 
    });
+
+   $("#logout").on("click", function(e){
+      //Clear User Info
+      localStorage.removeItem("user");
+      localStorage.removeItem("game");
+   });
+
+   var setupGameStatsRedirect = function(){
+      $(".game_item").on("click", function(e){
+         var game_id = $(this).attr("id");
+         //Storing Game
+         localStorage.setItem("game", game_id);
+      });
+   }
    
     
 });
